9a083bbc3102568310298631f9a92fa30cd3e870
atarwn <102351430+atarwn@users.noreply.github.com>
2024-04-04 21:49:25 +0300

    040424-1

--- footer.php
+++ footer.php
@@ -4,4 +4,4 @@
     }
 </style>
 
-<p class="footer_vlomx27D39gakrvwi92">[Connect] - Stable 040424-1 (Afterschool Nightly)</p>
+<p class="footer_vlomx27D39gakrvwi92">[Connect] - Betabuild 040424-1 (Afterschool Nightly)</p>
