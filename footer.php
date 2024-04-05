77ffad3d97c67c6b04a80648a9457650b22f13a6
atarwn <102351430+atarwn@users.noreply.github.com>
2024-04-05 22:44:53 +0300

    060424

--- footer.php
+++ footer.php
@@ -4,4 +4,4 @@
     }
 </style>
 
-<p class="footer_vlomx27D39gakrvwi92">[Connect] - Betabuild 060424 (Colorful)</p>
+<p class="footer_vlomx27D39gakrvwi92">[Connect] - Deltabuild 060424 (Colorful)</p>
