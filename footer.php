0b202e3c05d21fce8fe16d2a381d20ead86aec32
atarwn <102351430+atarwn@users.noreply.github.com>
2024-04-07 13:14:47 +0300

    070424

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="footer1">
-        <p style="text-align: right;">[Connect] - Deltabuild 060424-1 (Colorful)</p>
+        <p style="text-align: right;">[Connect] - Deltabuild 070424 (Center)</p>
     </div>
     <div class="footer2">
         <a href="http://c.qwa.su"><img src="imgs/gif.gif"></a><br>
