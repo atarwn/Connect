6c8f51034e39401b7779bbe44fc5757f26ff0d35
atarwn <102351430+atarwn@users.noreply.github.com>
2024-05-15 16:07:16 +0300

    150524

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="footer1">
-        <p style="text-align: right;">[Connect] - Stable 150524 (nomoremossclicks)</p>
+        <p style="text-align: right;">[Connect] - Deltabuild d150524 (nomoremossclicks)</p>
     </div>
     <div class="footer2">
         <a href="http://c.qwa.su"><img src="imgs/gif.gif"></a><br>
