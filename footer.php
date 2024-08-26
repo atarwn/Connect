4ccf19a9241d71f2a726df56717752b1db1364d9
atarwn <102351430+atarwn@users.noreply.github.com>
2024-08-26 10:39:52 +0500

    rm -rf

--- footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-    <div class="footer1">
-        <p style="text-align: right;">[Connect] - Deltabuild d150524 (nomoremossclicks)</p>
-    </div>
-    <div class="footer2">
-        <a href="http://c.qwa.su"><img src="imgs/gif.gif"></a><br>
-        <input type="text" readonly value='<a href="http://c.qwa.su"><img src="http://c.qwa.su/imgs/gif.gif"></a>'>
-    </div>
-</div>
